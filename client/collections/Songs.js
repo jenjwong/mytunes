3130082d4a22a3f1463f7b0b92d6d4488fbee024
jenjwong <jenjwong@gmail.com>
2016-05-05 12:34:45 -0700

    complete song queue view

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,8 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
+  model: SongModel,
+
   initialize: function() {
     $.ajax({
       url: 'https://api.parse.com/1/classes/songs/',
@@ -10,8 +12,6 @@ var Songs = Backbone.Collection.extend({
         this.add(data.results);
       }
     });
-  },
-
-  model: SongModel
+  }
 
 });
