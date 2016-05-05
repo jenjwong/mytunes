05bd21040d45393767c770d38fb6be22f727d997
jenjwong <jenjwong@gmail.com>
2016-05-04 17:33:06 -0700

    reorganize events in song queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,31 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('enqueue', this.add, this);
+    this.on('dequeue', this.remove, this);
+    this.on('ended', this.remove, this);
+
+    this.on('add', this.added, this);
+    this.on('remove', this.removed, this);
+  },
+
+  added: function() {
+    if (this.length === 1) { // only one song in the queue
+      this.playFirst();
+    }
+  },
+
+  removed: function() {
+    if (this.length > 0) { // at least one song in the queue
+      this.playFirst();
+    }
+  },
+
+  playFirst: function() {
+    var firstSong = this.at(0);
+    if (firstSong) {
+      firstSong.play();
+    }
   }
 
 });
