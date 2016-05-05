d28867fc921115b864df23fa7d25777fbd2738f7
jenjwong <jenjwong@gmail.com>
2016-05-05 11:19:54 -0700

    clean up Songs from parse on init

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,17 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
+  initialize: function() {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/songs/',
+      method: 'GET',
+      success: data => {
+        // populate collection with song list
+        this.add(data.results);
+      }
+    });
+  },
+
   model: SongModel
 
 });
