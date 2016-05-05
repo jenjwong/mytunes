4c334c955a640c4106ef0925329530dd88caae97
jenjwong <jenjwong@gmail.com>
2016-05-05 15:36:05 -0700

    replace $.ajax with Backbone.Collection.prototype.fetch

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -2,16 +2,14 @@
 var Songs = Backbone.Collection.extend({
 
   model: SongModel,
+  url: 'https://api.parse.com/1/classes/songs',
 
   initialize: function() {
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/songs/',
-      method: 'GET',
-      success: data => {
-        // populate collection with song list
-        this.add(data.results);
-      }
-    });
+    this.fetch();
+  },
+
+  parse: function(response) {
+    return response.results;
   }
 
 });
--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -5,7 +5,7 @@ var LibraryView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
-    this.collection.on('all', this.render, this);
+    this.collection.on('sync', this.render, this);
   },
 
   render: function() {
